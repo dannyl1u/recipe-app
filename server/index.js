7a23a34e49d44de6eee3ee903cfc440d77004d76
dannyl1u <dannyjialiliu@gmail.com>
2023-07-07 22:45:03 -0700

    dockerize

--- server/index.js
+++ server/index.js
@@ -4,68 +4,77 @@ const cors = require('cors');
 
 const app = express();
 
-const uri = "mongodb+srv://kiaanc:kiaanc@recipe.tbzhj84.mongodb.net/recipes?retryWrites=true&w=majority";
+const connectionString = "mongodb://localhost:27017/recipes";
 
-// Define Schema
-const RecipeSchema = new mongoose.Schema({
-  name: String,
-  ingredients: String,
-  directions: String,
-  dateModified: String,
-});
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
 
-const Recipe = mongoose.model('Recipe', RecipeSchema);
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// POST api/recipes - Create a new recipe
-app.post('/api/recipes', async (req, res) => {
-  const newRecipe = new Recipe(req.body);
-  const result = await newRecipe.save();
-  res.json(result);
-});
-
-// GET api/recipes - Get all recipes
-app.get('/api/recipes', async (req, res) => {
-  const recipes = await Recipe.find();
-  res.json(recipes);
-});
-
-// PUT api/recipes/:id - Update a recipe
-app.put('/api/recipes/:id', async (req, res) => {
-  const updatedRecipe = req.body;
-  const result = await Recipe.findByIdAndUpdate(req.params.id, updatedRecipe, { new: true }); // option {new: true} returns the updated document
-  res.json(result);
-});
-
-// DELETE api/recipes/:id - Delete a recipe
-app.delete('/api/recipes/:id', async (req, res) => {
-  const result = await Recipe.findByIdAndRemove(req.params.id);
-  res.json(result);
-});
-
-// Define port
-const PORT = process.env.PORT || 5000;
-
-// Connect to the MongoDB cluster and start the server
-const startServer = async () => {
-  try {
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    const RecipeSchema = new mongoose.Schema({
+      name: String,
+      ingredients: String,
+      directions: String,
+      dateModified: String,
     });
-    console.log("Connected to MongoDB");
 
-    // Start the server
+    const Recipe = mongoose.model('Recipe', RecipeSchema);
+
+    // Middleware
+    app.use(cors());
+    app.use(express.json());
+
+    // POST api/recipes - Create a new recipe
+    app.post('/api/recipes', async (req, res) => {
+      try {
+        const newRecipe = new Recipe(req.body);
+        const result = await newRecipe.save();
+        res.json(result);
+      } catch (error) {
+        console.error('Error creating recipe:', error);
+        res.status(500).json({ error: 'Failed to create recipe' });
+      }
+    });
+
+    // GET api/recipes - Get all recipes
+    app.get('/api/recipes', async (req, res) => {
+      try {
+        const recipes = await Recipe.find();
+        res.json(recipes);
+      } catch (error) {
+        console.error('Error retrieving recipes:', error);
+        res.status(500).json({ error: 'Failed to retrieve recipes' });
+      }
+    });
+
+    // PUT api/recipes/:id - Update a recipe
+    app.put('/api/recipes/:id', async (req, res) => {
+      try {
+        const updatedRecipe = req.body;
+        const result = await Recipe.findByIdAndUpdate(req.params.id, updatedRecipe, { new: true });
+        res.json(result);
+      } catch (error) {
+        console.error('Error updating recipe:', error);
+        res.status(500).json({ error: 'Failed to update recipe' });
+      }
+    });
+
+    // DELETE api/recipes/:id - Delete a recipe
+    app.delete('/api/recipes/:id', async (req, res) => {
+      try {
+        const result = await Recipe.findByIdAndRemove(req.params.id);
+        res.json(result);
+      } catch (error) {
+        console.error('Error deleting recipe:', error);
+        res.status(500).json({ error: 'Failed to delete recipe' });
+      }
+    });
+
+    const PORT = process.env.PORT || 5000;
+
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
-  } catch (err) {
-    console.log("Error connecting to MongoDB:", err);
-    process.exit(1);
-  }
-};
-
-startServer();
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
